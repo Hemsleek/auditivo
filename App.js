c2002639a222ef4f410b787918696a9a66557e49
hemsleek <asiyanbimubashir@gmail.com>
2020-12-02 23:59:39 +0100

    WIP: screen routes

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NativeRouter } from 'react-router-native';
+import { NativeRouter, Route } from 'react-router-native';
+
 
 import DefaultLayout from './app/layout/DefaultLayout';
 import Discover from './app/screens/Discover'
+import Feed from './app/screens/Feed'
+import Playlist from './app/screens/Playlist'
+import Search from './app/screens/Search'
 
 export default function App() {
   return (
     <NativeRouter>
       <DefaultLayout>
-        <Discover />
+        <Route path='/' exact component={<Discover />} />
+        <Route path='/feed' component={<Feed />} />
+        <Route path='/playlist' component={<Playlist />} />
+        <Route path='/search' component={<Search />} />
+
       </DefaultLayout>
     </NativeRouter>
   );
